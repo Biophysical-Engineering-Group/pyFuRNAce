3ca4e2999f8c775fab2a7423673a77be1cf3f827
Luca Monari <we297@uni-heidelberg.de>
2025-02-26 12:51:43 +0100

    changed the interface of the sticky notes in order to have a container for the motif menu

--- pyfurnace/app/pages/1_Design.py
+++ pyfurnace/app/pages/1_Design.py
@@ -12,35 +12,44 @@ if __name__ == "__main__":
     load_logo()
     ### initiate the session state
     des_func.initiate_session_state()
-    origami = st.session_state.origami
     st.write("## Design RNA origami")
 
     ### make the general options for the RNA origami
-    des_func.origami_general_options(origami, expanded=False)
+    des_func.origami_general_options(st.session_state.origami, 
+                                     expanded=False)
 
-    cols = st.columns([1] * 7)   
+    cols = st.columns([2.75] + [1] * 2 + [2] + [1.3])   
     with cols[0]:    
         with st.popover("Make a simple Origami",
                         help='Start by creating a simple Origami rather than starting from scratch'):
             des_func.simple_origami()
-    with cols[3]:
-        st.selectbox('OxView 3D ColorMap:', ['Reds', None] + plt.colormaps() , key='oxview_colormap', help='Change the color of the OxView visualization.')
-    with cols[-1]:
-        st.toggle('Gradient Stran Path', key='gradient', help='Toggle the gradient color scheme for the nucleotides')
+    with cols[1]:
+        st.write('OxView 3D ColorMap:')
+    with cols[2]:
+        st.selectbox('OxView 3D ColorMap:', 
+                     ['Reds', None] + plt.colormaps(),
+                     key='oxview_colormap',
+                     label_visibility='collapsed', 
+                     help='Change the color of the OxView visualization.')
+    with cols[4]:
+        st.toggle('Color Gradient Path', 
+                  key='gradient', 
+                  help='Toggle the gradient color scheme for the nucleotides')
 
 
     ### option menu to manage the motifs in the origami
-    st.write("#### Add Motifs to the Origami:")
+    # st.write("#### Add Motifs to the Origami:")
     # with st.popover(":green[Add Motifs to the Origami:]",
     #                 use_container_width=True,):
     #     make_motif_menu(origami)
     with sticky_container(mode="top", border=False):
-        des_func.make_motif_menu(origami)
-        # st.markdown("""<hr style="height:1px;border:none;color:#DDDDDD;background-color:#DDDDDD;" /> """, unsafe_allow_html=True)
-        # st.markdown("<hr style='margin-top:+0em;border:none;margin-bottom:-1em;color:#FFFFFFw;background-color:#FFFFFF;' />", unsafe_allow_html=True)
-        st.markdown("<hr style='margin-top:+0.5em;margin-bottom:+1.0em;' />", unsafe_allow_html=True)
+        with st.expander("**Add Motifs to the Origami:**", expanded=True):
+            des_func.make_motif_menu(st.session_state.origami)
+            # st.markdown("""<hr style="height:1px;border:none;color:#DDDDDD;background-color:#DDDDDD;" /> """, unsafe_allow_html=True)
+            # st.markdown("<hr style='margin-top:+0em;border:none;margin-bottom:-1em;color:#FFFFFFw;background-color:#FFFFFF;' />", unsafe_allow_html=True)
+            # st.markdown("<hr style='margin-top:+0.5em;margin-bottom:+1.0em;' />", unsafe_allow_html=True)        
+            # st.divider()
         view_opt = des_func.origami_select_display()
-        # st.divider()
 
 
     ### add separator

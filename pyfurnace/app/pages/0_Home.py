af117c695ce1a909079ae5c898c82810f3eda2b0
Luca Monari <we297@uni-heidelberg.de>
2025-07-03 22:54:58 +0200

    fixed line length

--- pyfurnace/app/pages/0_Home.py
+++ pyfurnace/app/pages/0_Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from utils import load_logo, app_path
+
+### pyFuRNAce modules
+from utils import load_logo
 
 if __name__ == '__main__':
     load_logo() 
@@ -26,8 +28,8 @@ if __name__ == '__main__':
                  label=":orange[Template:]", 
                  icon=":material/genetics:")
 
-    st.markdown("- Prepare the DNA template for you RNA Origami, search subsequences "
-                "and search for dimers.")
+    st.markdown("- Prepare the DNA template for you RNA Origami, search subsequences"
+                " and search for dimers.")
 
     st.page_link("pages/4_Prepare.py", 
                  label=":orange[Prepare:]", 
@@ -37,21 +39,32 @@ if __name__ == '__main__':
                 "OxDNA simulation.")
     
 
-    st.write("## About pyFuRNAce")
-    st.markdown("pyFuRNAce is an open-source Python package and web-based design engine for creating "
-                "complex RNA nanostructures using the co-transcriptional RNA origami approach.")
-    st.markdown(" - **GitHub**: [Biophysical-Engineering-Group/pyFuRNAce](https://github.com/Biophysical-Engineering-Group/pyFuRNAce)")
+    st.divider()
+
+    st.write("### About pyFuRNAce")
+    
+    st.markdown("pyFuRNAce is an open-source Python package and web-based design "
+                "engine for creating complex RNA nanostructures using the "
+                " co-transcriptional RNA origami approach.")
+    st.markdown(" - **GitHub**: [Biophysical-Engineering-Group/pyFuRNAce]"
+                "(https://github.com/Biophysical-Engineering-Group/pyFuRNAce)")
     st.markdown(" - **PyPI**: [pyfurnace](https://pypi.org/project/pyfurnace/)")
-    st.markdown(" - **Documentation**: [Read the Docs](https://pyfurnace.readthedocs.io/en/latest/)")
+    st.markdown(" - **Documentation**: [Read the Docs]"
+                "(https://pyfurnace.readthedocs.io/en/latest/)")
     st.markdown(" - bug reports, feature requests or any other questions, "
                 "please reach out to us via the " 
-                "[GitHub Issues](https://github.com/Biophysical-Engineering-Group/pyFuRNAce/issues)"
+                "[GitHub Issues](https://github.com/Biophysical-Engineering"
+                "-Group/pyFuRNAce/issues)"
                 " or the "
-                "[GitHub Discussions](https://github.com/Biophysical-Engineering-Group/pyFuRNAce/discussions)."
+                "[GitHub Discussions](https://github.com/Biophysical-Engineering"
+                "-Group/pyFuRNAce/discussions)."
                 )
     
+    st.divider()
+    
     st.write("#### Check out the 1-min demo video:")
-    st.video("https://github.com/Biophysical-Engineering-Group/pyFuRNAce/blob/main/vid/demo_1min.mp4?raw=true", 
+    st.video("https://github.com/Biophysical-Engineering-Group/pyFuRNAce/blob"
+             "/main/vid/demo_1min.mp4?raw=true", 
              format="video/mp4", 
              start_time=0, 
              subtitles=None, 

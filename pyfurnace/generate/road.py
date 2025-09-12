e36fac48cc17242419f4c775be5881eeb5a6bb74
Luca Monari <we297@uni-heidelberg.de>
2025-09-12 22:19:11 +0200

    added default KL energies

--- pyfurnace/generate/road.py
+++ pyfurnace/generate/road.py
@@ -129,6 +129,12 @@ def generate_road(
                 struct_list[pair_map[i]] = "}"
 
     ### ADD THE PSEUDOKNOTS ROAD NOTATION
+    avg_pk_E = 0
+    avg_pk_dE = 0
+    if not pseudoknots:
+        avg_pk_E = -9.0
+        avg_pk_dE = 1.81
+
     if isinstance(pseudoknots, dict):
         pk_dict = pseudoknots
     else:
@@ -146,8 +152,6 @@ def generate_road(
         "I": "9",
     }
     external_pk_count = 0
-    avg_pk_E = 0
-    avg_pk_dE = 0
 
     # sort the pseudoknots by their index
     pk_dict = {

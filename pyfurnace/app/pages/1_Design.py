08d96c23c7840e306409d0dbf59d2fcb86cb60b6
Luca Monari <we297@uni-heidelberg.de>
2025-10-28 02:02:33 +0100

    switching from motif menu in the sidebar to fake_sidebar with streamlit columns and sticky container, also using new st.container for horizontal alignment

--- pyfurnace/app/pages/1_Design.py
+++ pyfurnace/app/pages/1_Design.py
@@ -8,6 +8,12 @@ import utils.design_functions as des_func
 from utils.st_fixed_container import sticky_container
 
 
+# motif menu function
+def motif_menu_expander():
+    with st.expander("**Add motifs to the origami:**", expanded=True):
+        des_func.make_motif_menu(st.session_state.origami)
+
+
 if __name__ == "__main__":
 
     load_logo()
@@ -24,11 +30,14 @@ if __name__ == "__main__":
     ### make the general options for the RNA origami
     des_func.origami_general_options(st.session_state.origami, expanded=False)
 
-    ### make 3 common options for the RNA origami
-    cols = st.columns([1, 1, 1, 1], vertical_alignment="center")
+    with st.container(
+        horizontal=True,
+        vertical_alignment="center",
+        horizontal_alignment="distribute",
+        gap="large",
+    ):
 
-    # simple origami popover
-    with cols[0]:
+        # simple origami popover
         with st.popover(
             "Make a simple origami",
             use_container_width=False,
@@ -37,18 +46,33 @@ if __name__ == "__main__":
         ):
             des_func.simple_origami()
 
-    with cols[1]:
-        motif_menu_sidebar = st.toggle(
-            "Motif menu in the sidebar",
-            value=st_state.sidebar_motif_menu,
-            help="Show the motif menu in the sidebar "
-            "instead of below the general options.",
-        )
-        if motif_menu_sidebar != st_state.sidebar_motif_menu:
-            st_state.sidebar_motif_menu = motif_menu_sidebar
-            st.rerun()
+        # motif menu sidebar toggle
+        with st.container(horizontal_alignment="center"):
+            motif_menu_sidebar = st.toggle(
+                "Motif sidebar",
+                value=st_state.sidebar_motif_menu,
+                help="Show the motif menu in the sidebar "
+                "instead of below the general options.",
+            )
+            if motif_menu_sidebar != st_state.sidebar_motif_menu:
+                st_state.sidebar_motif_menu = motif_menu_sidebar
+                st.rerun()
 
-    with cols[2]:
+            motif_width = 0.01
+            if motif_menu_sidebar:
+                # make the slider to adjust the sidebar width
+                motif_width = st.slider(
+                    "Split sidebar width (%)",
+                    min_value=10,
+                    max_value=90,
+                    value=42,
+                    format="%d%% sidebar",
+                    label_visibility="collapsed",
+                    help="Adjust the width of the sidebar "
+                    "to better fit the motif menu.",
+                )
+
+        # colormap
         cmap = st.selectbox(
             "Colormap:",
             ["Reds", None] + plt.colormaps(),
@@ -58,8 +82,7 @@ if __name__ == "__main__":
         )
         st.session_state.oxview_colormap = cmap
 
-    # gradient toggle
-    with cols[3]:
+        # gradient toggle
         grad = st.toggle(
             "Color gradient path",
             key="grad",
@@ -67,49 +90,46 @@ if __name__ == "__main__":
         )
         st.session_state.gradient = grad
 
-    # motif menu function
-    def motif_menu_expander():
-        with st.expander("**Add motifs to the origami:**", expanded=True):
-            des_func.make_motif_menu(st.session_state.origami)
+    motif_sidebar, main = st.columns(
+        [motif_width / 100, 1 - motif_width / 100],
+        border=st_state.get("sidebar_motif_menu", False),
+    )
 
-    ### display the motif menu in the sidebar or as sticky container
-    if st.session_state.sidebar_motif_menu:
-        st.markdown(
-            """
-            <style>
-            [data-testid="stSidebar"][aria-expanded="true"]{
-                min-width: 30%;
-                max-width: 50%;
-            }
-            """,
-            unsafe_allow_html=True,
-        )
-        with st.sidebar:
-            st.markdown("#### :green[Drag the border of the sidebar to resize it ->]")
-            motif_menu_expander()
-        # if the motif menu is in the sidebar,
-        # keep the origami view menu sticky, so no need to scroll back up
-        with sticky_container(mode="top", border=False):
-            view_opt = des_func.origami_select_display()
-    else:
-        if st.session_state.motif_menu_sticky:
+    ### display the motif menu in the sidebar
+    if st_state.sidebar_motif_menu:
+        with motif_sidebar:
+            st.write("#### Motif Menu:")
             with sticky_container(mode="top", border=False):
+                des_func.make_motif_menu(st.session_state.origami)
+
+    with main:
+        # motif menu in the sidebar, so stick the origami view options
+        if st.session_state.sidebar_motif_menu:
+            with sticky_container(mode="top", border=False):
+                view_opt = des_func.origami_select_display()
+
+        # if the motif menu is not in the sidebar, display it here
+        else:
+            # sticky the motif menu
+            if st.session_state.motif_menu_sticky:
+                with sticky_container(mode="top", border=False):
+                    motif_menu_expander()
+                    view_opt = des_func.origami_select_display()
+            # normal motif menu
+            else:
                 motif_menu_expander()
                 view_opt = des_func.origami_select_display()
+
+        ### select the render mode
+        if not st.session_state.origami:
+            st.success("The origami is empty, add a motif!")
+            st.stop()
         else:
-            motif_menu_expander()
-            view_opt = des_func.origami_select_display()
+            des_func.origami_build_view(view_opt)
 
-    ### select the render mode
-    if not st.session_state.origami:
-        st.success("The origami is empty, add a motif!")
-        st.stop()
-    else:
-        des_func.origami_build_view(view_opt)
+        ### display dot-bracket structure, sequence constraints
+        # and link to the Generate page
+        des_func.display_structure_sequence()
 
-    ### display dot-bracket structure, sequence constraints
-    # and link to the Generate page
-    des_func.display_structure_sequence()
-
-    ### Download the RNA origami structure
-    save_origami()
+        ### Download the RNA origami structure
+        save_origami()
